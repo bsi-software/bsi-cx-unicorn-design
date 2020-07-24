39dc829fc26ba2ad9a4aee1c03cd2efdc6349bec
André Wegmüller <Andre.Wegmueller@bsi-software.com>
2020-07-24 17:17:35 +0200

    Added JS for navigation

--- js/main.js
+++ js/main.js
@@ -4,16 +4,51 @@
  */
 var bannerNo = 1;
 
-function animate() {
+function animateBanner() {
   bannerNo++;
   if (bannerNo >= 4) {
-	bannerNo = 1;
+    bannerNo = 1;
   }
   var $img = $('#banner-image');
   $img.attr('src', 'img/banner-' + bannerNo + '.png');
-  setTimeout(animate, 10 * 1000);
+  setTimeout(animateBanner, 10 * 1000);
 }
 
 $(document).ready(function() {
-  setTimeout(animate, 10 * 1000);
-});
+  setTimeout(animateBanner, 10 * 1000);
+  attachNavigation();
+})
+
+/* ---- NAVIGATION ---- */
+
+function attachNavigation() {
+  $('.navigation-folder').on('click', onNavigationFolderClick);
+}
+
+function onNavigationFolderClick(event) {
+  let $folder = $(event.currentTarget);
+  let $panel = $folder.parent().find('.navigation-item-container-lv2');
+  if ($panel.hasClass('navigation-visible')) {
+    hideNavigationFolderPanel($panel);
+    return;
+  }
+  let clickHandler = onNavigationFolderClickOutside.bind($panel);
+  $panel.addClass('navigation-visible navigation-fade-in');
+  $panel.data('clickHandler', clickHandler);
+  setTimeout(function() {
+    $(document).on('click', clickHandler);
+  });
+}
+
+function onNavigationFolderClickOutside(event) {
+  let $panel = this;
+  if ($panel[0] !== event.target) {
+    hideNavigationFolderPanel($panel);
+  }
+}
+
+function hideNavigationFolderPanel($panel) {
+  $panel.removeClass('navigation-visible');
+  $(document).off('click', $panel.data('clickHandler'));
+  $panel.removeData('clickHandler');
+}
