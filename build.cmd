7a5047d9743dcb27669a6a31040b73243286cc46
André Wegmüller <Andre.Wegmueller@bsi-software.com>
2020-08-12 16:18:55 +0200

    Updated build script for automatic design upload

--- build.cmd
+++ build.cmd
@@ -1,2 +1,11 @@
 set ZIP_PATH=C:\Program Files\7-Zip
 "%ZIP_PATH%\7z" a dist\unicorn-website-design.zip -xr!dist -x!design-preview.png
+
+set DESIGN_PATH=.\dist\unicorn-website-design.zip
+
+set SERVLET_URL="[copy/paste from running Studio application]"
+
+set SESSION_ID=[use JSESSIONID from Chrome's dev tools]
+
+curl --data-binary @%DESIGN_PATH% -H "Content-Type:application/octet-stream" --cookie "JSESSIONID=%SESSION_ID%" --trace-ascii dist/curl-debug.log %SERVLET_URL%
+
